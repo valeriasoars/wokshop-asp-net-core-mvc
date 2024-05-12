5457a61287e1d662430ac86c3e30f11197637c41
Valeria Soares <valeriasoaressantos2@gmail.com>
2024-05-12 12:30:57 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,12 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page";
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();

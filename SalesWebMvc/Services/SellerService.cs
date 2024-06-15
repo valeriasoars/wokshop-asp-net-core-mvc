a105c653a09ff19ad380c786742a1ea8d5f11c31
Valeria Soares <valeriasoaressantos2@gmail.com>
2024-06-15 15:55:38 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -40,7 +40,7 @@ namespace SalesWebMvc.Services
             }
             catch(DbUpdateException e)
             {
-                throw new IntegrityException(e.Message);
+                throw new IntegrityException("Can't delete seller because he/she has sales");
             }
         }
 
